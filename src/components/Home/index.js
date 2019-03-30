99c428605ab23dea620f645346f11f5762c767d9
Ali Movahedi <ali_movahedi@aol.com>
2019-03-30 13:15:01 +0430

    feat: add treejer's section to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Layout from 'Root/components/Layout';
 import styles from './index.less';
+import adoptATree from './adopt-a-tree.png';
+import earnOxygen from './earn-oxygen.png';
+import tradeYourOxygens from './trade-your-oxygens.png';
 
 export default () => (
   <Layout>
@@ -34,5 +37,87 @@ export default () => (
     </div>
 
     <div className={styles.divider} />
+
+    <div className={styles.treejernutshell}>
+      <p className="head">
+        TREEJER&apos;S PLANET IN A NUTSHELL
+      </p>
+
+      <div className="threethere">
+        <div className="content">
+          <img
+            src={adoptATree}
+            alt="adopt-a-tree"
+          />
+          <p className="title">
+            Adopt A Tree
+          </p>
+          <p className="details">
+            Adopt a real tree and add it to your
+            personal forest in the Planet
+          </p>
+          <Link
+            to="/know-more"
+            className="link"
+          >
+            KNOW MORE
+          </Link>
+        </div>
+        <div className="verticalDivider" />
+        <div className="content">
+          <img
+            src={earnOxygen}
+            alt="earn-oxygen"
+          />
+          <p className="title">
+            Earn Oxygen
+          </p>
+          <p className="details">
+            Get rewards for making a positive
+            impact on the environment
+          </p>
+          <Link
+            to="/know-more"
+            className="link"
+          >
+            KNOW MORE
+          </Link>
+        </div>
+        <div className="verticalDivider" />
+        <div className="content">
+          <img
+            src={tradeYourOxygens}
+            alt="trade-your-oxygens"
+          />
+          <p className="title">
+            Trade Your Oxygens
+            Earn Oxygen
+          </p>
+          <p className="details">
+            Exchange your earned Oxygen (O2) for
+            products, services or money!
+          </p>
+          <Link
+            to="/know-more"
+            className="link"
+          >
+            KNOW MORE
+          </Link>
+        </div>
+      </div>
+
+
+      <div className="treejerContainer">
+        <Link
+          to="/treejer-for-bussiness"
+          className="treejer"
+        >
+          Treejer for Business
+        </Link>
+      </div>
+    </div>
+
+    <div className={styles.divider} />
+
   </Layout>
 );
