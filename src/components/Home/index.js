85be4c442f5359436c5364f950a0c21bec79a8f5
Ali Movahedi <ali_movahedi@aol.com>
2019-03-30 12:27:36 +0430

    feat: add section telling story to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Layout from 'Root/components/Layout';
+import styles from './index.less';
 
 export default () => (
   <Layout>
-    Home
+    <div className={styles.tellingStory}>
+      <p className="head">
+        Telling the Story of One Planet
+      </p>
+      <p className="info">
+        In a world full of indifference, a grand movement is rising to stand for the future. You
+        might be the next link within this green chain. Adopt your unique tree and let locals plant
+        it!
+      </p>
+
+      <div className="buttons">
+        <Link
+          to="/go-to-planet"
+          className="goToPlanet"
+        >
+          Go To the Planet
+        </Link>
+        <div className="counter">
+          <p className="count">
+            1500 Trees
+          </p>
+          <p className="details">
+            adopted by Treejer users
+          </p>
+        </div>
+      </div>
+    </div>
   </Layout>
 );
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -8,7 +8,7 @@ export default props => (
     <div className={styles.header}>
       <div>
         <img
-          className={styles.logo}
+          className="logo"
           src={Logo}
           alt="logo"
         />
