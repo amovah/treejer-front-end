b362b403ad4cd55daada3ddf3922497503f38c58
Ali Movahedi <ali_movahedi@aol.com>
2019-03-30 10:48:26 +0430

    refactor: refactor main route and home component

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Switch404 from 'Root/shared/Switch404';
+import Home from './Home';
 import NotFound from './NotFound';
 
 export default () => (
   <Switch404>
-    <Route path="/home">
-      <p>
-        Home
-      </p>
-    </Route>
+    <Route path="/" exact component={Home} />
     <Route path="/404-notfound" component={NotFound} />
   </Switch404>
 );
