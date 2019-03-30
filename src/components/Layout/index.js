220b746e3e6b5012c37e87d6490f5d1c64373b3b
Ali Movahedi <ali_movahedi@aol.com>
2019-03-30 11:49:35 +0430

    feat: add header to layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from 'Root/assets/logo.png';
 import styles from './index.less';
 
 export default props => (
   <div className={styles.container}>
+    <div className={styles.header}>
+      <div>
+        <img
+          className={styles.logo}
+          src={Logo}
+          alt="logo"
+        />
+
+        <Link
+          to="/for-startups"
+        >
+          for startups
+        </Link>
+        <Link
+          to="/about"
+        >
+          about
+        </Link>
+        <Link
+          to="/contact"
+        >
+          contact
+        </Link>
+        <Link
+          to="/become-a-planter"
+          className="unique"
+        >
+          become a planter
+        </Link>
+      </div>
+
+      <div>
+        <Link
+          to="/login"
+          className="login"
+        >
+          Login
+        </Link>
+        <Link
+          to="/adopt-a-tree"
+          className="adopt"
+        >
+          adopt a tree
+        </Link>
+      </div>
+    </div>
+
     {props.children}
   </div>
 );
