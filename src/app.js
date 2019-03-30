3881f1ae9321f69a1663f745fec84a1470e634b9
Ali Movahedi <ali_movahedi@aol.com>
2019-03-30 09:53:31 +0430

    feat: add main component

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Treejer from './Treejer';
 
+render(
+  <Treejer />,
+  global.document.getElementById('root'),
+);
 
 if (module.hot) {
   module.hot.accept();
