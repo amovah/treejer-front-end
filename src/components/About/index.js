1dd543e12e28b258307c5fcebdfc5af2d2dfd05f
Ali Movahedi <ali_movahedi@aol.com>
2019-03-30 20:41:24 +0430

    feat: add illusterator box

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Layout from 'Root/components/Layout';
 import Footer from 'Root/components/Footer';
+import styles from './index.less';
 
 export default () => (
   <Layout>
-
+    <div className={styles.illusterate}>
+      Illusterate
+    </div>
     <Footer />
   </Layout>
 );
