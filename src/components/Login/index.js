830610c898a9a28117a562e63341e5b934f5b0df
Ali Movahedi <ali_movahedi@aol.com>
2019-03-31 23:36:17 +0430

    feat: add route to login page

---
+++ src/components/Login/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SignLayout from 'Root/components/SignLayout';
+
+export default () => (
+  <SignLayout>
+    A
+  </SignLayout>
+);
---
+++ src/components/SignLayout/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.less';
+
+export default props => (
+  <div>
+    {props.children}
+  </div>
+);
