9bdc4c826b9e2d6b988605f198fffd0e2cb54eb5
Ali Movahedi <ali_movahedi@aol.com>
2019-04-06 19:38:11 +0430

    feat: add step two to sign up

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,12 +1,87 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import SignLayout from 'Root/components/SignLayout';
 import Input from 'Root/components/Input';
 import styles from './index.less';
 
 export default class extends Component {
+  state = {
+    step: 1,
+  };
+
   onSignIn = () => {
   }
+
+  goToStepTwo = () => {
+    this.setState({
+      step: 2,
+    });
+  }
+
+  stepOne = () => (
+    <Fragment>
+      <Input
+        placeholder="Email Address"
+        style={{
+          width: '100%',
+          marginBottom: 30,
+        }}
+      />
+      <Input
+        placeholder="Password"
+        type="password"
+        style={{
+          width: '100%',
+          marginBottom: 30,
+        }}
+      />
+      <button
+        type="button"
+        className="button"
+        onClick={this.goToStepTwo}
+      >
+        Next
+      </button>
+    </Fragment>
+  )
+
+  stepTwo = () => (
+    <Fragment>
+      <div className="row">
+        <Input
+          placeholder="First Name"
+          type="text"
+          style={{
+            width: 'calc(50% - 10px)',
+            marginBottom: 30,
+          }}
+        />
+        <Input
+          placeholder="Last Name"
+          type="text"
+          style={{
+            width: 'calc(50% - 10px)',
+            marginBottom: 30,
+          }}
+        />
+      </div>
+      <Input
+        placeholder="Username"
+        type="text"
+        style={{
+          width: '100%',
+          marginBottom: 30,
+        }}
+      />
+      <button
+        type="button"
+        className="button"
+        onClick={this.goToStepTwo}
+      >
+        Sign Up
+      </button>
+    </Fragment>
+  );
 
   render() {
     return (
@@ -20,28 +95,7 @@ export default class extends Component {
             Please enter your details below
           </p>
 
-          <Input
-            placeholder="Email Address"
-            style={{
-              width: '100%',
-              marginBottom: 30,
-            }}
-          />
-          <Input
-            placeholder="Password"
-            type="password"
-            style={{
-              width: '100%',
-              marginBottom: 30,
-            }}
-          />
-          <button
-            type="button"
-            className="button"
-            onClick={this.onSignUp}
-          >
-            Next
-          </button>
+          {this.state.step === 1 ? this.stepOne() : this.stepTwo()}
 
           <div className="notMember">
             <span>
