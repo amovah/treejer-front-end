9fac0686a21465845bf720a8cdc9ac2a800764ee
Ali Movahedi <ali_movahedi@aol.com>
2019-04-08 18:27:11 +0430

    feat: user can navigate between steps

--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Layout from 'Root/shared/Layout';
 import Header from 'Root/shared/Header';
 import TopPadding from 'Root/shared/Header/TopPadding';
@@ -12,6 +12,70 @@ export default class extends Component {
     current: 1,
   };
 
+  handlePage = () => {
+    switch (this.state.current) {
+      case 1: {
+        return this.collectPage();
+      }
+
+      case 2: {
+        return this.reviewPage();
+      }
+
+      default: {
+        return this.collectPage();
+      }
+    }
+  }
+
+  collectPage = () => (
+    <Fragment>
+      <div>
+        <p>
+          a
+        </p>
+        <p>
+          a
+        </p>
+        <p>
+          a
+        </p>
+      </div>
+      <div className="box">
+        <img
+          src={logo}
+          alt="logo"
+        />
+        <p>
+          Did you know that your new trees can produce the oxygen needed for
+          <span className="green">&nbsp;50 people</span> per year over time?
+        </p>
+        <div className="divider" />
+        <p className="title">
+          Cash Total
+        </p>
+        <p className="price">
+          $173.00
+        </p>
+        <Button
+          onClick={() => {
+            this.setState({
+              current: 2,
+            });
+          }}
+        >
+          NEXT
+        </Button>
+      </div>
+    </Fragment>
+  )
+
+  reviewPage = () => (
+    <p>
+      review
+    </p>
+  )
+
   render() {
     return (
       <Layout>
@@ -21,37 +85,7 @@ export default class extends Component {
         <Steper current={this.state.current} />
 
         <div className={styles.container}>
-          <div>
-            <p>
-              a
-            </p>
-            <p>
-              a
-            </p>
-            <p>
-              a
-            </p>
-          </div>
-          <div className="box">
-            <img
-              src={logo}
-              alt="logo"
-            />
-            <p>
-              Did you know that your new trees can produce the oxygen needed for
-              <span className="green">&nbsp;50 people</span> per year over time?
-            </p>
-            <div className="divider" />
-            <p className="title">
-              Cash Total
-            </p>
-            <p className="price">
-              $173.00
-            </p>
-            <Button>
-              NEXT
-            </Button>
-          </div>
+          {this.handlePage()}
         </div>
       </Layout>
     );
