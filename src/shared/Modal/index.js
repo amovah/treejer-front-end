0e9b9ea572a1f0b2d215bf18dc43848f4ed43e01
Ali Movahedi <ali_movahedi@aol.com>
2019-04-10 13:49:18 +0430

    feat: clicking on modal background will close modal

--- src/shared/Modal/index.js
+++ src/shared/Modal/index.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import Box from 'Root/shared/Box';
+import closeModal from 'Root/actions/modal/close';
 import styles from './index.less';
 
 class Modal extends Component {
   container = React.createRef();
 
+  componentDidMount() {
+    this.container.current.addEventListener('click', (e) => {
+      if (e.target === this.container.current) {
+        closeModal();
+      }
+    });
+  }
+
   render() {
     return (
       <div
         className={classnames(styles.container, this.props.modal.show && 'active')}
-        onClick={() => { console.log('yea'); }}
+        ref={this.container}
       >
-
         <Box
           className="modal"
         >
