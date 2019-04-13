0d807fc697e436515101bc7e7a159942c7be792f
Ali Movahedi <ali_movahedi@aol.com>
2019-04-13 10:52:34 +0430

    feat: add Table to planet page

--- src/components/Planet/ForestView/index.js
+++ src/components/Planet/ForestView/index.js
@@ -1,8 +1,45 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import Box from 'Root/shared/Box';
+import Table from 'Root/shared/Table';
 import styles from './index.less';
 import PadDivider from '../PadDivider';
+
+const sampleData = [
+  [
+    <span className="text">
+      #123456789
+    </span>,
+    <span className="text">
+      Pending
+    </span>,
+    <span className="text">
+      22/12/2017, 6:39 PM
+    </span>,
+    <span className="value">
+      $170.00
+    </span>,
+  ],
+  [
+    <span className="text">
+      #123456789
+    </span>,
+    <span className="text">
+      Confirmed
+    </span>,
+    <span className="text">
+      22/12/2017, 6:39 PM
+    </span>,
+    <span className="value">
+      $170.00
+    </span>,
+  ],
+];
+const data = [];
+for (let i = 0; i < 10; i = i + 1) {
+  data.push(...sampleData);
+}
+
 
 const ForestView = (props) => {
   if (!props.user.logged) {
@@ -12,19 +49,64 @@ const ForestView = (props) => {
   if (!props.user.invited) {
     return (
       <Box className={styles.container}>
-        <p>
-          shit
+        <p className="title">
+          Payment History
         </p>
+        <div className="tableContainer">
+          <Table
+            data={data}
+            heads={['Order', 'Status', 'Date/Time', 'Payment']}
+            className={styles.table}
+          />
+        </div>
+        <div className="highShow">
+          <p>
+            Not Invited Yet
+          </p>
+        </div>
       </Box>
+    );
+  }
+
+  if (!props.user.trees) {
+    return (
+      <Fragment>
+        <Box className={styles.container}>
+          <p className="title">
+            Payment History
+          </p>
+          <div className="tableContainer">
+            <Table
+              data={data}
+              heads={['Order', 'Status', 'Date/Time', 'Payment']}
+              className={styles.table}
+            />
+          </div>
+          <div className="highShow">
+            <p>
+              You dont have tree mate
+            </p>
+          </div>
+        </Box>
+
+        <PadDivider gap={150} />
+      </Fragment>
     );
   }
 
   return (
     <Fragment>
       <Box className={styles.container}>
-        <p>
-          shit
+        <p className="title">
+          Payment History
         </p>
+        <div className="tableContainer">
+          <Table
+            data={data}
+            heads={['Order', 'Status', 'Date/Time', 'Payment']}
+            className={styles.table}
+          />
+        </div>
       </Box>
 
       <PadDivider gap={150} />
