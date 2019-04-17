b83685930854438558c31ecd941630b103f67571
Ali Movahedi <ali_movahedi@aol.com>
2019-04-17 08:50:06 +0430

    feat: add reducer to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from 'redux';
 import user from './user';
 import modal from './modal';
 import claim from './claim';
+import trees from './trees';
+import receipts from './receipts';
 
 export default combineReducers({
   user,
   modal,
   claim,
+  trees,
+  receipts,
 });
