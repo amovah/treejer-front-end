b740f005fb8de002867571b4198368c6e8e737a3
Ali Movahedi <ali_movahedi@aol.com>
2019-04-18 21:19:36 +0430

    feat: redirect all route to earth landing page

--- src/components/index.js
+++ src/components/index.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import Switch404 from 'Root/shared/Switch404';
-import Home from './Home';
-import NotFound from './NotFound';
-import About from './About';
-import Contact from './Contact';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-import Confirm from './Confirm';
-import Order from './Order';
-import Planet from './Planet';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Earth from './Earth';
 
 export default () => (
-  <Switch404>
-    <Route path="/" exact component={Home} />
-    <Route path="/about" exact component={About} />
-    <Route path="/contact" exact component={Contact} />
-    <Route path="/sign-in" component={SignIn} />
-    <Route path="/sign-up" component={SignUp} />
-    <Route path="/confirm/:code" exact component={Confirm} />
-    <Route path="/order" exact component={Order} />
-    <Route path="/planet" exact component={Planet} />
+  <Switch>
     <Route path="/earth-day-2019" exact component={Earth} />
-    <Route path="/404-notfound" component={NotFound} />
-  </Switch404>
+    <Route>
+      <Redirect to="/earth-day-2019" />
+    </Route>
+  </Switch>
 );
