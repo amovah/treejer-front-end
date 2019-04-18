0b175e53ce11429e6a7c8a6e0ae0623a679539e2
Ali Movahedi <ali_movahedi@aol.com>
2019-04-18 19:02:58 +0430

    feat: left side styles

--- src/components/Earth/index.js
+++ src/components/Earth/index.js
@@ -19,20 +19,30 @@ export default () => (
       />
     </div>
 
-    <div className={styles.container}>
-      <p
-        className="title"
-      >
+    <p
+      className={styles.title}
+    >
         This Earth Day pledge to plant a tree!
-      </p>
-
-      <div className="content">
-        <div className="left">
-          a
-        </div>
-        <div className="right">
-          b
-        </div>
+    </p>
+    <div className={styles.container}>
+      <div className="left">
+        <p
+          className="text"
+        >
+          Let’s all get together in this. Even more so than funds, we are raising awareness
+          towards the existing
+          threats on our planet and our eco-sysytems worldwide. We are launching our
+          reforestation and tree
+          plantation project called ‘Treejer’. It’s a tree planting community created by
+          investors and volunteers.
+          Using concepts such as social forestry, tokenomics and gamification, our
+          international project aims
+          to inspire various groups to join an inclusive and sustainable model of tree
+          plantation.
+        </p>
+      </div>
+      <div className="right">
+        b
       </div>
     </div>
   </Layout>
