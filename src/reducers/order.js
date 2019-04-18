6986078898fc9d136f54677ca0456dab44f66a20
Ali Movahedi <ali_movahedi@aol.com>
2019-04-18 11:22:53 +0430

    chore: remvoe test action in order reducer

--- src/reducers/order.js
+++ src/reducers/order.js
@@ -77,10 +77,6 @@ export default (state = defaults, action) => {
       return defaults;
     }
 
-    case 'a': {
-      return JSON.parse("{\"count\":4,\"trees\":[{\"type\":\"Quercus\",\"price\":100,\"region\":\"Planet\",\"drive\":\"B Site\",\"o2RatePerDay\":25,\"id\":\"treeNo2\",\"qty\":3},{\"type\":\"Populus\",\"price\":100,\"region\":\"Planet\",\"drive\":\"A Site\",\"o2RatePerDay\":15,\"id\":\"treeNo1\",\"qty\":1}]}");
-    }
-
     default: {
       return state;
     }
