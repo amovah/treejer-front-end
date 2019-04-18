2325403bd379490129561959826ebc6df4c5d620
Ali Movahedi <ali_movahedi@aol.com>
2019-04-18 19:10:11 +0430

    feat: add text in left side

--- src/components/Earth/index.js
+++ src/components/Earth/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Layout from 'Root/shared/Layout';
 import Header from 'Root/shared/Header';
 import TopPadding from 'Root/shared/Header/TopPadding';
@@ -39,6 +40,57 @@ export default () => (
           international project aims
           to inspire various groups to join an inclusive and sustainable model of tree
           plantation.
+        </p>
+        <p className="title">
+          How to support?
+        </p>
+        <p
+          className="text"
+        >
+          At the moment we are accepting donations to support the conception stage of the project
+          to take it
+          to an MVP. Your donations will help us plant trees and validate our plantation model
+          with planting
+          and preservation cycles for volunteer communities who participate and sustain the drive.
+        </p>
+        <p
+          className="title"
+        >
+          Who plants the trees?
+        </p>
+        <p
+          className="text"
+        >
+          Voluntary groups of five members in local communities participating to start a Plantation
+          Drive in
+          available regions. These community groups are paid in crypto, so they can earn and keep
+          their
+          income even if they have no access to a bank. However, they have the option to change
+          their
+          earnings to fiat money.
+        </p>
+        <p
+          className="title"
+        >
+          About Treejer
+        </p>
+        <p
+          className="text"
+        >
+          Treejer is a Blockchain startup offering financial and non-financial incentives to
+          individuals,
+          businesses and governments to plant and preserve trees. Using concepts such as social
+          forestry,
+          tokenomics and gamification, this international project aims to inspire various groups
+          to join an
+          inclusive and sustainable model of tree plantation.
+          <br />
+          <Link
+            href="/read-more"
+            className="link"
+          >
+            READ MORE
+          </Link>
         </p>
       </div>
       <div className="right">
