9dc47995b02b4a9f46cd3a0a1fa3ca8420d049ba
Ali Movahedi <ali_movahedi@aol.com>
2019-04-18 18:46:11 +0430

    feat: add about pic to Earth file

--- src/components/Earth/index.js
+++ src/components/Earth/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Layout from 'Root/shared/Layout';
 import Header from 'Root/shared/Header';
+import TopPadding from 'Root/shared/Header/TopPadding';
+import { cdn } from 'Root/config';
+import styles from './index.less';
 
 export default () => (
   <Layout>
     <Header />
+    <TopPadding gap={200} />
+
+    <div
+      className={styles.aboutPic}
+    >
+      <img
+        src={`${cdn}/about.png`}
+        alt="about"
+      />
+    </div>
 
     <p>
       a
