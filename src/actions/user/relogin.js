21b2614c433c64fda06bf0ba4c3f2c04f98f9dff
Ali Movahedi <ali_movahedi@aol.com>
2019-04-18 12:03:46 +0430

    fix: fix relogin process

--- src/actions/user/relogin.js
+++ src/actions/user/relogin.js
@@ -4,14 +4,6 @@ import fetch from 'Root/fetch';
 
 export default async () => {
   if (global.localStorage.token) {
-    store.dispatch({
-      type: types.user.LOGIN,
-      data: {
-        token: global.localStorage.token,
-        id: global.localStorage.userId,
-      },
-    });
-
     const details = await fetch(`/clients/${global.localStorage.userId}`, {
       filter: {
         include: ['trees', 'receipts'],
@@ -19,8 +11,12 @@ export default async () => {
     });
 
     store.dispatch({
-      type: types.user.CHANGE,
-      toChange: details.data,
+      type: types.user.LOGIN,
+      data: {
+        ...details.data,
+        token: global.localStorage.token,
+        id: global.localStorage.userId,
+      },
     });
   }
 };
