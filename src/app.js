d69a537e7b7c5d31fe970ea522f21d60ce64e798
Ali Movahedi <ali_movahedi@aol.com>
2019-04-20 11:13:59 +0430

    feat: send notification every 6 hours

--- src/app.js
+++ src/app.js
@@ -3,10 +3,28 @@ import './web.config';
 import React from 'react';
 import { render } from 'react-dom';
 import relogin from 'Root/actions/user/relogin';
+import sendNotification from 'Root/actions/notifications/send';
 import Treejer from './Treejer';
 
 (async () => {
   await relogin();
+
+  if (global.localStorage.lastNotified) {
+    const time = parseInt(global.localStorage.lastNotified, 10);
+    if (Date.now() - time >= 1000 * 60 * 60 * 6) {
+      sendNotification({
+        type: 'warning',
+        text: 'My Apologies',
+      }, false);
+      global.localStorage.lastNotified = Date.now().toString();
+    }
+  } else {
+    global.localStorage.lastNotified = Date.now().toString();
+    sendNotification({
+      type: 'warning',
+      text: 'My Apologies',
+    }, false);
+  }
 
   render(
     <Treejer />,
