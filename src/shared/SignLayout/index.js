ca80325a3a365f399ff59fd87d5cdb0eb8ac0f96
Ali Movahedi <ali_movahedi@aol.com>
2019-04-21 19:30:53 +0430

    fix: fix links in SignLayout

--- src/shared/SignLayout/index.js
+++ src/shared/SignLayout/index.js
@@ -17,39 +17,51 @@ export default props => (
           individuals, businesses and governments to plant and preserve trees.
         </p>
         <Link
-          to="/know-more"
+          to="/about"
           className="knowMore"
         >
           know more
         </Link>
         <div className="socials">
           <div>
-            <img
-              src={`${cdn}/socials/instagram-black.png`}
-              alt="instagram"
-            />
-            <img
-              src={`${cdn}/socials/twitter-black.png`}
-              alt="twitter"
-            />
-            <img
-              src={`${cdn}/socials/linkedin-black.png`}
-              alt="linkedin"
-            />
+            <a href="https://www.instagram.com/treejer">
+              <img
+                src={`${cdn}/socials/instagram-black.png`}
+                alt="instagram"
+              />
+            </a>
+            <a href="https://twitter.com/Treejertalks">
+              <img
+                src={`${cdn}/socials/twitter-black.png`}
+                alt="twitter"
+              />
+            </a>
+            <a href="https://www.linkedin.com/company/treejer">
+              <img
+                src={`${cdn}/socials/linkedin-black.png`}
+                alt="linkedin"
+              />
+            </a>
           </div>
           <div>
-            <img
-              src={`${cdn}/socials/facebook-black.png`}
-              alt="facebook"
-            />
-            <img
-              src={`${cdn}/socials/medium-black.png`}
-              alt="medium"
-            />
-            <img
-              src={`${cdn}/socials/telegram-black.png`}
-              alt="telegram"
-            />
+            <a href="https://www.facebook.com/treejertalks">
+              <img
+                src={`${cdn}/socials/facebook-black.png`}
+                alt="facebook"
+              />
+            </a>
+            <a href="https://medium.com/@treejer">
+              <img
+                src={`${cdn}/socials/medium-black.png`}
+                alt="medium"
+              />
+            </a>
+            <a href="http://t.me/treejer">
+              <img
+                src={`${cdn}/socials/telegram-black.png`}
+                alt="telegram"
+              />
+            </a>
           </div>
         </div>
       </div>
